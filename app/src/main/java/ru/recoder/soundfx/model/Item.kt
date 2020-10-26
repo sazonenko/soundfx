c7db4796f364dc33579d54435f1e2cf7d3902b2a
Ivan Sazonenko <SazonenkoIA@kadastr.ru>
2020-10-26 17:22:41 +0700

    sub-cat from web

---
+++ app/src/main/java/ru/recoder/soundfx/model/Item.kt
@@ -0,0 +1,11 @@
+package ru.recoder.soundfx.model
+
+open class Item(protected val name : String) {
+    override fun toString(): String {
+        return name;
+    }
+}
+
+class WebItem(name : String, val url : String) : Item(name)
+
+class FileItem(name : String, val path : String) : Item(name)
--- app/src/main/java/ru/recoder/soundfx/ui/main/WebListFragment.kt
+++ app/src/main/java/ru/recoder/soundfx/ui/main/WebListFragment.kt
@@ -1,16 +1,22 @@
 package ru.recoder.soundfx.ui.main
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.ListFragment
 import org.jsoup.select.Elements
+import ru.recoder.soundfx.model.WebItem
 
 class WebListFragment : ListFragment() {
 
-    private var adapter : ArrayAdapter<String>? = null
-    private var currentData : ArrayList<String> = ArrayList()
+    private var adapter : ArrayAdapter<WebItem>? = null
+    private var currentData : ArrayList<WebItem> = ArrayList()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -18,29 +24,48 @@ class WebListFragment : ListFragment() {
                 activity!!,
                 android.R.layout.simple_list_item_1, currentData
         )
-        listAdapter = adapter
-        Thread {
-            val categories = loadCategories()
-            activity?.runOnUiThread { updateWithNewData(categories) }
+        loadData { loadCategories("https://noisefx.ru/", ".entry-content li a") }
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(javaClass.name, "view created ${Environment.getExternalStorageDirectory()}")
+    }
+
+    override fun onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
+        super.onListItemClick(listView, view, position, id)
+        Toast.makeText(activity, "Вы выбрали : $position) ${currentData[position]}", Toast.LENGTH_SHORT).show()
+        loadData{ loadCategories(currentData[position].url, ".v a.hre b") }
+    }
+    fun loadData(getList: () -> List<WebItem>) {
+        Thread {
+            try {
+                val result = getList()
+                activity?.runOnUiThread { updateWithNewData(result) }
+            } catch (e: Exception) {
+                activity?.runOnUiThread {
+                    Log.e(javaClass.name, "Error loading data", e)
+                    Toast.makeText(activity, "Error loading data: $e", Toast.LENGTH_LONG).show()
+                }
+            }
         }.start()
     }
 
-    fun loadCategories() : List<String> {
-        val result : ArrayList<String> = ArrayList()
-        try {
-            val doc: Document = Jsoup.connect("https://noisefx.ru/").get()
-            val elements: Elements = doc.select(".entry-content li a")
-            result.addAll( elements.map { e -> e.text() } )
-        } catch (e: Exception) {
-            println("error loading data: $e")
-        }
+    fun loadCategories(url : String, selector : String) : List<WebItem> {
+        Log.d(javaClass.name, "before load $url")
+        val result : ArrayList<WebItem> = ArrayList()
+        val doc: Document = Jsoup.connect(url).get()
+        val elements: Elements = doc.select(selector)
+        result.addAll( elements.map {
+            e -> WebItem(e.text(), e.absUrl("href"))
+        })
         return result
     }
 
-    fun updateWithNewData(result: List<String>) {
+    fun updateWithNewData(result: List<WebItem>) {
         currentData.clear()
         currentData.addAll(result)
         adapter?.notifyDataSetChanged()
+        listAdapter = adapter
     }
 }

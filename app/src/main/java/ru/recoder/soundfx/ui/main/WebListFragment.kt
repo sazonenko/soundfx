3651adec4ff140cbf74f2e64c3f1459f3e617b81
Ivan Sazonenko <SazonenkoIA@kadastr.ru>
2020-10-26 09:38:34 +0700

    cat-list from web

--- app/src/main/java/ru/recoder/soundfx/ui/main/WebListFragment.kt
+++ app/src/main/java/ru/recoder/soundfx/ui/main/WebListFragment.kt
@@ -7,34 +7,40 @@ import android.widget.ArrayAdapter
 import androidx.fragment.app.ListFragment
 import org.jsoup.select.Elements
 
-public class WebListFragment : ListFragment() {
+class WebListFragment : ListFragment() {
 
     private var adapter : ArrayAdapter<String>? = null
+    private var currentData : ArrayList<String> = ArrayList()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         adapter = ArrayAdapter(
                 activity!!,
-                android.R.layout.simple_list_item_1, getCategories()
+                android.R.layout.simple_list_item_1, currentData
         )
         listAdapter = adapter
+        Thread {
+            val categories = loadCategories()
+            activity?.runOnUiThread { updateWithNewData(categories) }
+
+        }.start()
     }
 
-    fun getCategories() : List<String> {
-        val doc: Document = Jsoup.connect("https://noisefx.ru/").get()
-        println(doc.title())
-        val elements: Elements = doc.select(".entry-content li a")
-
-/*
-        val list : ArrayList<String> =  ArrayList(elements.size)
-        for (headline in elements) {
-            list.add(headline.text())
-            println( String.format( "%s\n\t%s",
-                    headline.text(), headline.absUrl("href")
-            ))
+    fun loadCategories() : List<String> {
+        val result : ArrayList<String> = ArrayList()
+        try {
+            val doc: Document = Jsoup.connect("https://noisefx.ru/").get()
+            val elements: Elements = doc.select(".entry-content li a")
+            result.addAll( elements.map { e -> e.text() } )
+        } catch (e: Exception) {
+            println("error loading data: $e")
         }
-*/
-        return elements.map { e -> e.text()}
+        return result
     }
 
+    fun updateWithNewData(result: List<String>) {
+        currentData.clear()
+        currentData.addAll(result)
+        adapter?.notifyDataSetChanged()
+    }
 }
